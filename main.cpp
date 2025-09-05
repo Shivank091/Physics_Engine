20becede20edeb511569d3cc1e33bfa64da8ba9b
Shivank091 <Shivank091@users.noreply.github.com>
2025-09-05 14:43:44 +0530

    Implemented 1-D physics engine ,no collisons

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by plaso on 05-09-2025.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by plaso on 05-09-2025.
+//
+
+#ifndef PHYSICS_ENGINE_MAIN_H
+#define PHYSICS_ENGINE_MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //PHYSICS_ENGINE_MAIN_H
